488782108291594ec5920c6288e37fedef246c87
snehayendurineu <yenduri.s@northeastern.edu>
2024-07-20 19:11:05 -0400

    completed base backend and strated ui

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import {config} from 'dotenv'
 import morgan from 'morgan'
 import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 config();
 const app=express();
 
 //middlewares
 app.use(express.json())
-
+app.use(cookieParser(process.env.COOKIE_SECRET))
 //for loggin purpose, can remove it in higher production
 app.use(morgan("dev"))
 
